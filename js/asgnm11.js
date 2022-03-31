05eed77a4304fc1f0361e71a317225410ed6d993
MPerry00 <89421735+MPerry00@users.noreply.github.com>
2022-03-31 15:26:42 -0600

    submit

--- js/asgnm11.js
+++ js/asgnm11.js
@@ -3,32 +3,21 @@ para.addEventListener('click', updateName);
 
 function updateName () {
   const name = prompt('Enter a new name');
-  para.textContent = `Name: ${name}`
+  para.textContent = `Name: ${name}`;
 };
 
 
-
-const info = document.getElementByID("inputIceCream");
-para.addEventListener('click', updateFlavor);
+const info = document.getElementById("inputIceCream");
+info.addEventListener('click', updateFlavor);
 
 function updateFlavor () {
   const flavor = prompt('Enter favorite ice cream flavor');
-  para.textContent = `Favorite flavor: ${flavor}`
+  info.textContent = `Favorite flavor: ${flavor}`
 };
-/*
-if(iceCream === 'chocolate') {
-  alert('Thats my favorite too!');
-}
-  else {
-  alert('Awww, but chocolate is my favorite...');
-} */
 
 
-
-const travel = document.getElementById("inputDestination");
-para.addEventListener('click', updateDestination);
-
-function updateDestination () {
-  const place = prompt('Enter favorite destination');
-  para.textContent = `Destination: ${place}`
-}
+const bc = document.getElementById("buttonClick");
+bc.addEventListener('click', function onClick() {
+  bc.style.backgroundColor = '#09F0FF';
+  bc.style.color = '#092AFF';
+});
