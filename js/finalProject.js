8f80a8e4c3f94eb20dd15c3efd64d611cafbd81f
MPerry00 <89421735+MPerry00@users.noreply.github.com>
2022-05-03 11:51:08 -0600

    f6

--- js/finalProject.js
+++ js/finalProject.js
@@ -4,7 +4,7 @@ var randomColor = Math.floor(Math.random()*16777215).toString(16);
 
 const setBg = () => {
   const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  document.body.style.borderColor = "#" + randomColor;
+  document.body.style.backgroundColor = "#" + randomColor;
   color.innerHTML = "#" + randomColor;
 }
 
