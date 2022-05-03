8ed5eb51583a420e788e847516eba53e35586322
MPerry00 <89421735+MPerry00@users.noreply.github.com>
2022-05-03 11:20:43 -0600

    e

--- js/finalProject.js
+++ js/finalProject.js
@@ -1,8 +1,13 @@
-const quoteButton = document.querySelector('#js-new-quote');
+const quoteButton = document.querySelector('#js-new-color');
 quotebutton.addEventListener('click', getColor)
 
 const getColor() {
   const randomColor = Math.floor(Math.random()*16777215).toString(16);
   document.body.style.backgroundColor = "#" + randomColor;
   color.innerHTML = "#" + randomColor;
+}
+
+function displayColor(colorVal) {
+  const quoteText = document.querySelector('#js-color-text');
+  quoteText.textContent = colorVal;
 }
